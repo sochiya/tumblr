41a2406e8f41322e63858da11b97d2cd50bcd266
sochiya <mochi.kana514+sochiya@gmail.com>
2016-10-16 02:35:40 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+ruby '2.3.0'
+
+source 'https://rubygems.org' do
+  gem 'sass', '3.4.22'
+end
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp      = require('gulp');
+var sass      = require('gulp-ruby-sass');
+var plumber   = require('gulp-plumber');
+var pleeease  = require('gulp-pleeease');
+var jade      = require('gulp-jade');
+var pug       = require('gulp-pug');
+
+// sass
+gulp.task('sass', function () {
+  return sass('src/sass/', { bundleExec: true })
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(plumber())
+    .pipe(gulp.dest('public/assets/css'));
+});
+
+// pug
+gulp.task('views', function buildHTML() {
+  return gulp.src('src/pug/*.pug')
+  .pipe(pug({ pretty: true }))
+  .pipe(gulp.dest('public'))
+});
+
+// jade
+gulp.task('jade', function () {
+  gulp.src('src/jade/**/*.jade')
+    .pipe(plumber())
+    .pipe(jade())
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('copy', function() {
+  gulp.src('public/assets/css/*.css')
+    .pipe(gulp.dest('docs/css/'));
+});
+
+// watch
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.sass', ['sass']);
+  gulp.watch('src/jade/**/*.jade', ['views']);
+});
